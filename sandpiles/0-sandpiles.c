5d018b787d5b3a314cdf0128110f72113725b97b
chloe0524 <chloco31@gmail.com>
2024-09-24 10:49:35 +0200

    Added stable_and_print function to stabilize grid in separate function

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,25 +1,53 @@
-#include <stdio.h>
 #include "sandpiles.h"
+#include <stdio.h>
 
 /**
- * print_grid - prints 3x3 grid
- * @grid: grid to print
+ * stable_and_print - prints the current state of the grid and stabilizes it.
+ * @grid: A 3x3 grid representing the sandpile.
  */
-static void print_grid(int grid[3][3])
-{
-    int i, j;
 
-    printf("=\n");
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+void stable_and_print(int grid[3][3])
+{
+	int sand, grains;
+	int temp[3][3];
+
+	printf("=\n");
+	for (sand = 0; sand < 3; sand++)
+	{
+		for (grains = 0; grains < 3; grains++)
+		{
+			if (grains != 0)
+			{
+				printf(" ");
+			}
+			printf("%d", grid[sand][grains]);
+			temp[sand][grains] = grid[sand][grains];
+		}
+		printf("\n");
+	}
+
+	for (sand = 0; sand < 3; sand++)
+	{
+		for (grains = 0; grains < 3; grains++)
+		{
+			if (grid[sand][grains] >= 4)
+			{
+				temp[sand][grains] -= 4;
+				temp[sand - 1][grains] += (sand != 0) ? 1 : 0;
+				temp[sand + 1][grains] += (sand != 2) ? 1 : 0;
+				temp[sand][grains - 1] += (grains != 0) ? 1 : 0;
+				temp[sand][grains + 1] += (grains != 2) ? 1 : 0;
+			}
+		}
+	}
+
+	for (sand = 0; sand < 3; sand++)
+	{
+		for (grains = 0; grains < 3; grains++)
+		{
+			grid[sand][grains] = temp[sand][grains];
+		}
+	}
 }
 
 /**
@@ -28,46 +56,44 @@ static void print_grid(int grid[3][3])
  * @grid2: 2nd sandpile
  * Return: void
  */
+
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int sand, grains;
-    int falling_sand;
+	int sand, grains;
+	int stable;
 
-    for (sand = 0; sand < 3; sand++)
-    {
-        for (grains = 0; grains < 3; grains++)
-        {
-            grid1[sand][grains] += grid2[sand][grains];
-        }
-    }
-    print_grid(grid1);
+	for (sand = 0; sand < 3; sand++)
+	{
+		for (grains = 0; grains < 3; grains++)
+		{
+			grid1[sand][grains] += grid2[sand][grains];
+		}
+	}
 
-    do {
-        falling_sand = 0;
-        int temp[3][3] = {0};
+	while (1)
+	{
+		stable = 1;
+		for (sand = 0; sand < 3; sand++)
+		{
+			for (grains = 0; grains < 3; grains++)
+			{
+				if (grid1[sand][grains] >= 4)
+				{
+					stable = 0;
+					break;
+				}
+			}
+			if (stable == 0)
+			{
+				break;
+			}
+		}
 
-        for (sand = 0; sand < 3; sand++)
-        {
-            for (grains = 0; grains < 3; grains++)
-            {
-                if (grid1[sand][grains] >= 4)
-                {
-                    falling_sand = 1;
-                    temp[sand][grains] -= 4;
-                    if (sand > 0) temp[sand - 1][grains]++;
-                    if (sand < 2) temp[sand + 1][grains]++;
-                    if (grains > 0) temp[sand][grains - 1]++;
-                    if (grains < 2) temp[sand][grains + 1]++;
-                }
-            }
-        }
-        for (sand = 0; sand < 3; sand++)
-        {
-            for (grains = 0; grains < 3; grains++)
-            {
-                grid1[sand][grains] += temp[sand][grains];
-            }
-        }
-        falling_sand ? print_grid(grid1) : (void)0;
-    } while (falling_sand);
+		if (stable == 1)
+		{
+			break;
+		}
+
+		stable_and_print(grid1);
+	}
 }

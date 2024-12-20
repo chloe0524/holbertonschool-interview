d986a9d979f00de59da7547bf3ecd6fd96060663
chloe0524 <chloco31@gmail.com>
2024-12-20 09:33:38 +0100

    function in C that checks if a singly linked list is a palindrome

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "lists.h"
 
-
 /**
  * add_nodeint_begin - adds a new node pointing to previous one
  * @tail: pointer to pointer of latest node of listint_t list
@@ -11,59 +10,65 @@
  */
 listint_t *add_nodeint_begin(listint_t **tail, const int n)
 {
-	listint_t *new;
+    listint_t *new;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
 
-	new->n = n;
-	new->next = *tail;
-	*tail = new;
-	return (*tail);
+    new->n = n;
+    new->next = *tail;
+    *tail = new;
+    return (*tail);
 }
 
 /**
- * is_palindrome - prints all elements of a listint_t list
+ * is_palindrome - checks if a linked list is a palindrome
  * @head: pointer to head of list
  * Return: 1 if palindrome, 0 if not, -1 if allocation fails
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *tail = NULL;
-	listint_t *current_head, *current_tail, *to_be_freed;
-	int ret_code = 1;
+    listint_t *tail = NULL;  // Initialize tail to NULL
+    listint_t *current_head, *current_tail, *node_to_free;
+    int ret_code = 1;
 
-	/* empty list or only one value: is a palindrome */
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
+    // Empty list or only one value: is a palindrome
+    if (*head == NULL || (*head)->next == NULL)
+        return (1);
 
-	tail = malloc(sizeof(listint_t));
-	if (tail == NULL)
-		return (-1); /* Not handled */
+    // Create list with new nodes pointing to previous ones
+    current_head = *head;
+    while (current_head != NULL)
+    {
+        if (!add_nodeint_begin(&tail, current_head->n))
+        {
+            // Free allocated memory on failure
+            while (tail != NULL)
+            {
+                node_to_free = tail;
+                tail = tail->next;
+                free(node_to_free);
+            }
+            return (-1);
+        }
+        current_head = current_head->next;
+    }
 
-	/* Create list with new nodes pointing to previouses */
-	current_head = *head;
-	while (current_head != NULL)
-	{
-		add_nodeint_begin(&tail, current_head->n);
-		current_head = current_head->next;
-	}
+    // Compare original list and reversed list
+    current_head = *head;
+    current_tail = tail;
+    while (current_head != NULL)
+    {
+        // If different: not a palindrome
+        if (current_head->n != current_tail->n)
+            ret_code = 0;
+        current_head = current_head->next;
 
-	current_head = *head;
-	current_tail = tail;
-	while (current_head != NULL)
-	{
-		/*printf("Debug : head=%i  tail=%i\n", current_head->n, current_tail->n);*/
-		/* if different : not a palindrome */
-		if (current_head->n != current_tail->n)
-			ret_code = 0;
-		current_head = current_head->next;
+        node_to_free = current_tail;
+        current_tail = current_tail->next;
+        free(node_to_free);
+    }
 
-		to_be_freed = current_tail;
-		current_tail = current_tail->next;
-		free(to_be_freed);
-	}
-	free(current_tail);
-	return (ret_code);
+    return (ret_code);
 }

21d564eeb94daa1af15afdcf13d2065b91d56e90
chloe0524 <chloco31@gmail.com>
2025-01-30 15:41:09 +0100

    method that determines if a given data set represents a valid UTF-8 encoding

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Determines if a data set represents a valid UTF-8 encoding."""
 
+
 def validUTF8(data):
     """Determines if a data set represents a valid UTF-8 encoding."""
     num_bs = 0
     for b in data:
         b = b & 0xFF
-        
+
         if num_bs == 0:
             if (b >> 5) == 0b110:
                 num_bs = 1
@@ -20,5 +21,5 @@ def validUTF8(data):
             if (b >> 6) != 0b10:
                 return False
             num_bs -= 1
-    
+
     return num_bs == 0

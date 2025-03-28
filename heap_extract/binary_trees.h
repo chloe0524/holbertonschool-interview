6b66cf98c4fd0a91f46828e6f4178b006db7c107
chloe0524 <chloco31@gmail.com>
2025-03-28 09:57:22 +0100

    forgot to add ack

--- heap_extract/binary_trees.h
+++ heap_extract/binary_trees.h
@@ -1,22 +1,28 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 #include <stddef.h>
 #include <stdlib.h>
 
-/* Structure Definition */
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-} heap_t;
+        int n;
+        struct binary_tree_s *parent;
+        struct binary_tree_s *left;
+        struct binary_tree_s *right;
+} binary_tree_t;
 
-/* Function Prototypes */
+typedef struct binary_tree_s heap_t; /* ADD THIS LINE */
 int heap_extract(heap_t **root);
-heap_t *get_last_node(heap_t *root);
-void heapify_down(heap_t *tree);
+void binary_tree_print(const binary_tree_t *);
 
-#endif /* BINARY_TREES_H */
+#endif /* _BINARY_TREES_H_ */
 

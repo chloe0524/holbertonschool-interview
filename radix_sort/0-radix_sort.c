ceec3dc5e86caefa086c77a03658127035c3d23c
chloe0524 <chloco31@gmail.com>
2025-05-09 11:13:45 +0200

    radis redix

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -10,10 +10,11 @@
  */
 int get_max(int *array, size_t size)
 {
-    int max = array[0];
-    for (size_t i = 1; i < size; i++)
-        max = (array[i] > max) ? array[i] : max;
-    return (max);
+	int max = array[0];
+
+	for (size_t i = 1; i < size; i++)
+		max = (array[i] > max) ? array[i] : max;
+	return (max);
 }
 
 /**
@@ -24,28 +25,28 @@ int get_max(int *array, size_t size)
  */
 void counting_sort(int *array, size_t size, int exp)
 {
-    int *output = malloc(size * sizeof(int));
-    int count[10] = {0};
+	int *output = malloc(size * sizeof(int));
+	int count[10] = {0};
 
-    if (!output)
-        return;
+	if (!output)
+		return;
 
-    for (size_t i = 0; i < size; i++)
-        count[(array[i] / exp) % 10]++;
+	for (size_t i = 0; i < size; i++)
+		count[(array[i] / exp) % 10]++;
 
-    for (int i = 1; i < 10; i++)
-        count[i] += count[i - 1];
+	for (int i = 1; i < 10; i++)
+		count[i] += count[i - 1];
 
-    for (ssize_t i = size - 1; i >= 0; i--)
-    {
-        output[count[(array[i] / exp) % 10] - 1] = array[i];
-        count[(array[i] / exp) % 10]--;
-    }
+	for (ssize_t i = size - 1; i >= 0; i--)
+	{
+		output[count[(array[i] / exp) % 10] - 1] = array[i];
+		count[(array[i] / exp) % 10]--;
+	}
 
-    for (size_t i = 0; i < size; i++)
-        array[i] = output[i];
+	for (size_t i = 0; i < size; i++)
+		array[i] = output[i];
 
-    free(output);
+	free(output);
 }
 
 /**
@@ -55,14 +56,14 @@ void counting_sort(int *array, size_t size, int exp)
  */
 void radix_sort(int *array, size_t size)
 {
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    int max = get_max(array, size);
+	int max = get_max(array, size);
 
-    for (int exp = 1; max / exp > 0; exp *= 10)
-    {
-        counting_sort(array, size, exp);
-        print_array(array, size);
-    }
+	for (int exp = 1; max / exp > 0; exp *= 10)
+	{
+		counting_sort(array, size, exp);
+		print_array(array, size);
+	}
 }

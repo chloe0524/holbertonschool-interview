1da5d4f429f8ded54d5760ba93b6a564b21836d7
chloe0524 <chloco31@gmail.com>
2025-07-26 22:28:25 +0200

    fix interpreter

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -2,9 +2,9 @@
 """
 prints total size + status codes seen so far
 every 10 lines + when user hits ctrl + c
+only lines that match the format are counted
 if status code not valid --> we just skip it
 """
-
 
 import sys
 import re
@@ -22,38 +22,51 @@ def print_stats():
         print(f"{code}: {status_codes[code]}")
 
 
-if __name__ == "__main__":
-    try:
-        log_pattern = re.compile(
-            r'^'
-            r'\d+'
-            r'\.'
-            r'\d+'
-            r'\.'
-            r'\d+'
-            r' '
-            r'-'
-            r' '
-            r'\[.*?\]'
-            r' '
-            r'(\d{3}) '
-            r'(\d+)$'
-        )
+try:
+    # Are those lines short enough for you now pycodestyle?
+    # 'LiNe tOo lOnG (80 > 79 ChArS)' yeah right bye.
+    log_pattern = re.compile(
+        r'^'
+        r'\d'
+        r'+'
+        r'\.'
+        r'\d'
+        r'+'
+        r'\.'
+        r'\d+'
+        r' '
+        r'-'
+        r' '
+        r'\[.*?\]'
+        r' '
+        r'C'
+        r'h'
+        r'l'
+        r'o'
+        r'e'
+        r' '
+        r'C'
+        r' '
+        r'"GET /projects/260 HTTP/1.1" '
+        r'(\d{3}) '
+        r'(\d+)$'
+    )
 
-        for lin in sys.stdin:
-            match = log_pattern.match(lin.strip())
-            if match:
-                code, size = match.groups()
-                size = int(size)
-                total_size += size
-                if code in {'200', '301', '400', '401', '403',
-                            '404', '405', '500'}:
-                    status_codes[code] += 1
-            cnt += 1
-            if cnt % 10 == 0:
-                print_stats()
-    except KeyboardInterrupt:
-        print_stats()
-        raise
-    else:
-        print_stats()
+    for lin in sys.stdin:
+        match = log_pattern.match(lin.strip())
+        if match:
+            code, size = match.groups()
+            size = int(size)
+            total_size += size
+            if code in {
+                '200', '301', '400', '401', '403', '404', '405', '500'
+            }:
+                status_codes[code] += 1
+        cnt += 1
+        if cnt % 10 == 0:
+            print_stats()
+except KeyboardInterrupt:
+    print_stats()
+    raise
+else:
+    print_stats()

8254fd443306944e0782799358963f9666f18020
chloe0524 <chloco31@gmail.com>
2025-07-26 22:18:39 +0200

    moved comment

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -5,55 +5,46 @@ if status code not valid --> we just skip it
 """
 
 import sys
-import re
-from collections import defaultdict
-
-status_codes = defaultdict(int)
-total_size = 0
-cnt = 0
-
-
-def print_stats():
-    """print collected stats"""
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes):
-        print(f"{code}: {status_codes[code]}")
-
 
 if __name__ == "__main__":
-    try:
-        log_pattern = re.compile(
-            r'^'
-            r'\d+'
-            r'\.'
-            r'\d+'
-            r'\.'
-            r'\d+'
-            r' '
-            r'-'
-            r' '
-            r'\[.*?\]'
-            r' '
-            r'Chloe C '
-            r'"GET /projects/260 HTTP/1.1" '
-            r'(\d{3}) '
-            r'(\d+)$'
-        )
+    valid_status_codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
+    line_count = 0
+    total_file_size = 0
 
-        for lin in sys.stdin:
-            match = log_pattern.match(lin.strip())
-            if match:
-                code, size = match.groups()
-                size = int(size)
-                total_size += size
-                if code in {'200', '301', '400', '401', '403',
-                            '404', '405', '500'}:
-                    status_codes[code] += 1
-            cnt += 1
-            if cnt % 10 == 0:
+    def parse_log_line(line):
+        """Read, parse and extract status code and file size"""
+        try:
+            parts = line.split()
+            status_code = parts[-2]
+            if status_code in valid_status_codes:
+                valid_status_codes[status_code] += 1
+            return int(parts[-1])
+        except Exception:
+            return 0
+
+    def print_stats():
+        """Print file size and status code counts in ascending order"""
+        print("File size: {}".format(total_file_size))
+        for code in sorted(valid_status_codes.keys()):
+            if valid_status_codes[code]:
+                print(f"{code}: {valid_status_codes[code]}")
+
+    try:
+        for line in sys.stdin:
+            total_file_size += parse_log_line(line)
+            line_count += 1
+            if line_count % 10 == 0:
                 print_stats()
     except KeyboardInterrupt:
         print_stats()
         raise
-    else:
-        print_stats()
+    print_stats()

87d98fb99a9da3291f47d3545c26c2596fd2f2f6
chloe0524 <chloco31@gmail.com>
2025-09-12 14:01:12 +0200

    betty style fix

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -1,4 +1,3 @@
-#include "menger.h"
 
 /**
  * is_empty - Recursively checks if a coordinate is in a "hole" of the sponge
@@ -8,14 +7,14 @@
  */
 int is_empty(int x, int y)
 {
-    while (x > 0 || y > 0)
-    {
-        if (x % 3 == 1 && y % 3 == 1)
-            return (1);
-        x /= 3;
-        y /= 3;
-    }
-    return (0);
+	while (x > 0 || y > 0)
+	{
+		if (x % 3 == 1 && y % 3 == 1)
+			return (1);
+		x /= 3;
+		y /= 3;
+	}
+	return (0);
 }
 
 /**
@@ -24,22 +23,22 @@ int is_empty(int x, int y)
  */
 void menger(int level)
 {
-    int size, x, y;
+	int size, x, y;
 
-    if (level < 0)
-        return;
+	if (level < 0)
+		return;
 
-    size = (int)pow(3, level);
+	size = (int)pow(3, level);
 
-    for (y = 0; y < size; y++)
-    {
-        for (x = 0; x < size; x++)
-        {
-            if (is_empty(x, y))
-                printf(" ");
-            else
-                printf("#");
-        }
-        printf("\n");
-    }
+	for (y = 0; y < size; y++)
+	{
+		for (x = 0; x < size; x++)
+		{
+			if (is_empty(x, y))
+				printf(" ");
+			else
+				printf("#");
+		}
+		printf("\n");
+	}
 }
